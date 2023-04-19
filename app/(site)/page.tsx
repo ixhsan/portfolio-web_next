83a4204b8a502bff6bf3fbf115b3b9db9a00f81b
ixhsan <muharipan@gmail.com>
2023-04-19 16:48:55 +0700

    initial commit

---
+++ app/(site)/page.tsx
@@ -0,0 +1,47 @@
+import { getProjects } from "@/sanity/sanity-utils";
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function Home() {
+  const projects = await getProjects();
+
+  return (
+    <div>
+      <h1 className="text-7xl font-extrabold">
+        Hello I&apos;m{" "}
+        <span className="bg-gradient-to-r from-yellow-400 via-green-500 to-blue-600 bg-clip-text text-transparent">
+          Ikhsan
+        </span>
+        !
+      </h1>
+
+      <p className="mt-3 text-xl text-gray-500">
+        Hello everyone! Check out my projects!
+      </p>
+
+      <h2 className="mt-24 font-bold text-gray-700 text-3xl">My projects</h2>
+      <div className="mt-5 grid grid-cold-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {projects.map((project) => (
+          <Link
+            key={project._id}
+            className="border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition"
+            href={`/projects/${project.slug}`}
+          >
+            {project.image && (
+              <Image
+                src={project.image}
+                alt={project.name}
+                width={750}
+                height={300}
+                className="object-cover rounded-lg border border-gray-500"
+              />
+            )}
+            <div className="mt-2 font-extrabold bg-gradient-to-r from-yellow-400 via-green-500 to-blue-600 bg-clip-text text-transparent">
+              {project.name}
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ sanity/config/client-config.ts
@@ -0,0 +1,7 @@
+const config = {
+  projectId: "uupjd18j",
+  dataset: "production",
+  apiVersion: "2023-04-18",
+};
+
+export default config;
